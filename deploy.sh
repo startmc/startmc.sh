6e2d44c57c80043be8e8de3a1fb184a789a40fbb
kadenscott <kscottdev@gmail.com>
2020-11-28 17:14:16 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,6 @@ git add -A
 
 git commit -m "deploy"
 
-git push -f github.com/startmc/startmc.github.io master:gh-pages
+git push -f https://github.com/startmc/startmc.github.io master:gh-pages
 
 cd -
