442823387d767bdd7d23c3fe259c5d8902b0a302
kadenscott <kscottdev@gmail.com>
2020-11-29 18:47:32 -0500

    Add nice footer links

--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -1,9 +1,11 @@
 <template>
   <footer class="footer-sticky bg-4 p-2">
     <div class="content has-text-centered">
-      <h3 class="subtitle has-text-white has-text-centered"><a href="https://tadhg.sh" target="_blank">aber</a> & <a href="https://kaden.sh" target="_blank">blue</a></h3>
-      <span><img src="https://cdn.discordapp.com/emojis/761876733372399627.png" alt=":pog:" height="30px" width="30px"></span>
-      <h6 class="subtitle has-text-white has-text-centered">Thanks to <a href="https://aikar.co/">Aikar</a> and his fantastic <a href="https://aikar.co/2018/07/02/tuning-the-jvm-g1gc-garbage-collector-flags-for-minecraft/">flags</a></h6>
+      <h3 class="subtitle has-text-white has-text-centered footer-text"><a href="https://tadhg.sh" target="_blank">aber</a> & <a href="https://kaden.sh" target="_blank">blue</a></h3>
+      <span class="footer-text"><img src="https://cdn.discordapp.com/emojis/761876733372399627.png" alt=":pog:" height="30px" width="30px"></span>
+      <h6 class="footer-text subtitle has-text-white has-text-centered">Thanks to <a href="https://aikar.co/">Aikar</a> and his fantastic <a href="https://aikar.co/2018/07/02/tuning-the-jvm-g1gc-garbage-collector-flags-for-minecraft/">flags</a></h6>
+      <h6 class="footer-text subtitle has-text-white has-text-centered">Need more help? Check out the <a href="https://www.reddit.com/r/syscraft/">/r/syscraft</a> <a href=" https://discord.gg/Dx6SSkx">Discord community</a>.</h6>
+      <h6 class="footer-text subtitle has-text-white has-text-centered">Does your server run <a href="https://papermc.io/">Paper</a>? It should.</h6>
     </div>
   </footer>
 </template>
@@ -20,6 +22,11 @@
   bottom: 0;
   width: 100%;
   text-align: center;
+}
+
+.footer-text {
+  margin-top: 0.3em;
+  margin-bottom: 0.3em;
 }
 
 .content a {
