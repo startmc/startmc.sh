697bb7731744383e9564d7de70e85930fa2aab71
kadenscott <kscottdev@gmail.com>
2020-11-29 00:32:54 -0500

    Improve script generation logic

--- src/components/pages/PageMain.vue
+++ src/components/pages/PageMain.vue
@@ -12,15 +12,9 @@
             </div>
           </div>
           <div class="field column">
-            <label class="label" for="minRam">Min RAM allocation</label>
+            <label class="label" for="ram">Min RAM allocation</label>
             <div class="control">
-              <input class="input" type="text" placeholder="10G" id="minRam">
-            </div>
-          </div>
-          <div class="field column">
-            <label class="label" for="maxRam">Max RAM allocation</label>
-            <div class="control">
-              <input class="input" type="text" placeholder="10G" id="maxRam">
+              <input class="input" type="text" placeholder="10G" id="ram">
             </div>
           </div>
           <div class="field column">
@@ -54,34 +48,17 @@ export default {
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-  document.getElementById('minRam').addEventListener("input", regenCode);
-  document.getElementById('maxRam').addEventListener("input", regenCode);
+  document.getElementById('ram').addEventListener("input", regenCode);
   document.getElementById('jarFileName').addEventListener("input", regenCode);
   document.getElementById('gui').addEventListener("change", regenCode);
 });
 
-function minRam() {
-  let minRam = document.getElementById('minRam').value;
-  if (minRam == '') {
-    minRam = '10G';
-  }
-  return minRam;
-}
-
-function maxRam() {
-  let maxRam = document.getElementById('maxRam').value;
-  if (maxRam == '') {
-    maxRam = '10G';
-  }
-  return maxRam;
+function getRam() {
+  return document.getElementById('ram').value;
 }
 
 function getJarFileName() {
-  let jarFileName = document.getElementById('jarFileName').value;
-  if (jarFileName == '') {
-    jarFileName = 'yatopia_1-16-4.jar';
-  }
-  return jarFileName;
+  return document.getElementById('jarFileName').value;
 }
 
 function getGui() {
@@ -90,10 +67,17 @@ function getGui() {
 
 function regenCode() {
   console.log('regenning..... xddd hi blue')
-  let code = `java -Xms${minRam()} -Xmx${maxRam()} -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar ${getJarFileName()}`;
-  if (getGui()) {
-    code += ' nogui';
+
+  let ram = getRam();
+  let isGuiEnabled = getGui();
+  let jarName = getJarFileName();
+
+  if (ram === '' || jarName === '') {
+    return;
   }
+
+  let code = `java -Xms${ram} -Xmx${ram} -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar ${jarName}${isGuiEnabled ? ' nogui' : ''}`;
+
   document.querySelector('#script').innerHTML = code;
 }
 </script>
