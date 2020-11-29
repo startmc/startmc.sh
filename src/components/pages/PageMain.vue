c7110b286f90b4d9a6de08ee8b6c620a6e8b7a8f
kadenscott <kscottdev@gmail.com>
2020-11-29 10:18:44 -0500

    Improve popup code

--- src/components/pages/PageMain.vue
+++ src/components/pages/PageMain.vue
@@ -29,10 +29,10 @@
     <div class="column">
       <div class="box">
         <h1 class="subtitle">💾 Copy your start script</h1>
-        <pre class="p-0 m-0 script-container">
+        <pre class="p-0 m-0 script-container" id="script-container">
           <code class="p-0 m-0 script" id="script">Waiting for input...</code>
         </pre>
-        <p class="py-2 is-size-6">This might look scary, but don't worry. You can click on a flag to learn more about
+        <p class="py-2 is-size-6">This might look scary, but don't worry. You can hover over a flag to learn more about
           it's purpose!</p>
       </div>
     </div>
@@ -41,14 +41,14 @@
         <h1 class="subtitle">🖥️ Start your server!</h1>
       </div>
     </div>
-    <article class="message is-hidden flag-popup" id="popup">
-      <div class="message-header">
-        <p>Hello World</p>
-      </div>
-      <div class="message-body">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. <strong>Pellentesque risus mi</strong>, tempus quis placerat ut, porta nec nulla. Vestibulum rhoncus ac ex sit amet fringilla. Nullam gravida purus diam, et dictum <a>felis venenatis</a> efficitur. Aenean ac <em>eleifend lacus</em>, in mollis lectus. Donec sodales, arcu et sollicitudin porttitor, tortor urna tempor ligula, id porttitor mi magna a neque. Donec dui urna, vehicula et sem eget, facilisis sodales sem.
-      </div>
-    </article>
+    <div class="flag-popup hide" id="popup">
+      <article class="message">
+        <div class="message-header">
+        </div>
+        <div class="message-body">
+        </div>
+      </article>
+    </div>
   </div>
 </template>
 
@@ -84,9 +84,40 @@ let scriptElement;
 
 document.addEventListener("DOMContentLoaded", function () {
   scriptElement = document.getElementById("script");
+
   document.getElementById('ram').addEventListener("input", regenCode);
   document.getElementById('jarFileName').addEventListener("input", regenCode);
   document.getElementById('gui').addEventListener("change", regenCode);
+
+  document.addEventListener('mousemove', (e) => {
+    let element = e.target;
+    let popup = document.getElementById('popup')
+
+    if (element.classList.contains("flag")) {
+      let flag = element.innerHTML.trim()
+
+      let description = flags[flag];
+
+      let popup = document.getElementById("popup")
+
+      let headerElement = popup.children[0].children[0]
+
+      let bodyElement = popup.children[0].children[1]
+      headerElement.innerHTML = flag
+
+      bodyElement.innerHTML = description
+
+      popup.classList.remove("hide")
+
+      let x = e.clientX;
+      let y = e.clientY;
+
+      popup.style.top = y + "px"
+      popup.style.left = x + "px"
+    } else {
+      popup.classList.add("hide")
+    }
+  })
 });
 
 function getRam() {
@@ -99,33 +130,6 @@ function getJarFileName() {
 
 function getGui() {
   return document.getElementById("gui").checked
-}
-
-function hideDescription() {
-  let popup = document.getElementById("popup")
-  popup.classList.add("is-hidden")
-}
-
-function showDescription(event) {
-  let element = event.target
-
-  let flag = element.innerHTML
-
-  let description = flags[flag];
-
-  let popup = document.getElementById("popup")
-
-  let headerElement = popup.children[0]
-
-  let bodyElement = popup.children[1]
-  headerElement.innerHTML = flag
-
-  bodyElement.innerHTML = description
-
-  popup.classList.remove("is-hidden")
-  popup.style.position = "absolute"
-  popup.style.top = event.screenY+"px"
-  popup.style.left = event.screenX+"px"
 }
 
 function regenCode() {
@@ -149,30 +153,32 @@ function regenCode() {
 
   scriptElement.innerHTML = prefix
 
+  // eslint-disable-next-line no-unused-vars
   let i = 0;
 
-  for (var flag in flags) {
+  for (let flag in flags) {
     i++;
     console.log(flag)
-    let flagElement = document.createElement("spawn")
+    let flagElement = document.createElement("span")
+    flagElement.style.whiteSpace = 'nowrap'
     flagElement.id = flag
-    flagElement.innerText = flag+" "
-    flagElement.style.color = i % 2 === 0 ? "#343434 !important" : "#1a1a1a !important"
+    flagElement.innerText = flag + " "
+    flagElement.classList.add("flag")
+
+    if (i % 2 === 0) {
+      flagElement.style.color = "#382e94"
+    } else {
+      flagElement.style.color = "#000000"
+    }
 
     scriptElement.append(flagElement)
 
     let newElement = document.getElementById(flag);
 
     console.log(newElement)
-
-    newElement.addEventListener('mouseover', showDescription)
-    newElement.addEventListener('mouseout', hideDescription)
   }
 
   scriptElement.append(suffixElement)
-
-  //let code = `java -Xms${ram} -Xmx${ram} -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar ${jarName}${isGuiEnabled ? ' nogui' : ''}`;
-//  document.querySelector('#script').innerHTML = code;
 }
 </script>
 
@@ -182,16 +188,29 @@ function regenCode() {
 }
 
 .flag-popup {
-  min-width: 20em;
+  width: 24em;
+  min-height: 5em;
+  position: absolute;
 }
 
 .script {
   word-wrap: break-word;
   word-break: break-word;
-  white-space: normal;
+  white-space: pre-line;
 }
 
-.script-container {
-  word-wrap: break-word;
+.message-header {
+  white-space: nowrap;
+  word-break: keep-all;
 }
+
+#popup {
+  opacity: 1;
+  transition: opacity 0.1s linear;
+}
+
+.hide {
+  opacity: 0 !important;
+}
+
 </style>
