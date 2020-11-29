b94b370c11ee6a28ebb8c6dba71742a39c119654
kadenscott <kscottdev@gmail.com>
2020-11-29 18:43:50 -0500

    Add links to aikar

--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -3,6 +3,7 @@
     <div class="content has-text-centered">
       <h3 class="subtitle has-text-white has-text-centered"><a href="https://tadhg.sh" target="_blank">aber</a> & <a href="https://kaden.sh" target="_blank">blue</a></h3>
       <span><img src="https://cdn.discordapp.com/emojis/761876733372399627.png" alt=":pog:" height="30px" width="30px"></span>
+      <h6 class="subtitle has-text-white has-text-centered">Thanks to <a href="https://aikar.co/">Aikar</a> and his fantastic <a href="https://aikar.co/2018/07/02/tuning-the-jvm-g1gc-garbage-collector-flags-for-minecraft/">flags</a></h6>
     </div>
   </footer>
 </template>
@@ -19,5 +20,9 @@
   bottom: 0;
   width: 100%;
   text-align: center;
+}
+
+.content a {
+  color: #ffcad3;
 }
 </style>
