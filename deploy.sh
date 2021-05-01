f877948c920c0e3a099190f3194dfbcaa3b5eef0
stijnb1234 <stijnbannink23@gmail.com>
2021-05-01 17:37:02 +0200

    Updated deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,21 @@
 #!/usr/bin/env sh
+
 # abort on errors
 set -e
+
 # build
-yarn run build
-# navigate into the build output directory
+npm run build
+
 cd dist
-# if you are deploying to a custom domain
+
 # echo 'www.example.com' > CNAME
+
 git init
+
 git add -A
+
 git commit -m 'deploy'
+
 git push -f git@github.com:stijnb1234/startmc.sh.git master:gh-pages
+
 cd -
