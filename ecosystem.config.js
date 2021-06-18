81991da192d2136cd2da4a7625b1c659fc2a97cf
Kaden Scott <kscottdev@gmail.com>
2021-06-18 03:02:35 -0400

    update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,6 +4,6 @@ module.exports = {
         PM2_SERVE_PATH: './dist',
         PM2_SERVE_PORT: 3001,
         PM2_SERVE_SPA: 'true',
-        PM2_SERVE_HOMEPAGE: './dist/index.html'
+        PM2_SERVE_HOMEPAGE: 'index.html'
     }
 }
