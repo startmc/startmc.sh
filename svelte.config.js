9165fcbf2773f656c0c187c30d14718e2a997dcf
Kaden Scott <kscottdev@gmail.com>
2021-06-19 08:57:25 -0400

    fix adapter nonsense

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,15 @@
 import adapter from '@sveltejs/adapter-static'
 
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
     kit: {
         // hydrate the <div id="svelte"> element in src/app.html
         target: '#svelte',
-        adapter: adapter({out: 'dist'})
+        adapter: adapter({
+            // default options are shown
+            pages: 'dist',
+            assets: 'dist',
+            fallback: null
+        })
     }
 };
 
