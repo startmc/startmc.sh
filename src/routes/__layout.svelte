ce50c781858d4b39c4dc3883d49c6356abc3d872
Kaden Scott <kscottdev@gmail.com>
2021-06-19 09:09:16 -0400

    add layout

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,16 @@
+<script>
+    import '../app.css'
+
+    import Header from '$lib/Header/index.svelte'
+</script>
+
+<div class="layout">
+    <Header />
+</div>
+
+<style>
+    .layout {
+        display: flex;
+        min-height: 100vh;
+    }
+</style>
