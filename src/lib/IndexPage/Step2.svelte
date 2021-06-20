fdc5a43545ef5b6b02d3d4349ae5b7b3126e67f3
Kaden Scott <kscottdev@gmail.com>
2021-06-20 13:32:05 -0400

    split out steps on index route to separate components

---
+++ src/lib/IndexPage/Step2.svelte
@@ -0,0 +1,38 @@
+<script>
+    import StepCard from '$lib/StepCard/index.svelte'
+</script>
+
+<StepCard title="💾 Generate your start script">
+</StepCard>
+
+<style>
+    .forms {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        padding: 0.2em;
+    }
+
+    .form {
+        display: flex;
+        flex-direction: column;
+        flex: 1;
+        margin-inline: 1em;
+    }
+
+    .input {
+        margin: 0.3em 0;
+        border: 1px solid rgb(170, 170, 170);
+        border-radius: 0.2em;
+        padding: 0.3em;
+        font-size: 1em;
+    }
+
+    .input:focus {
+        border: 1px solid var(--bg-light-alt)
+    }
+
+    .help {
+        margin: 0;
+    }
+</style>
