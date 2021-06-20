ec6682f51804398c399e17d0cd5c3631180003da
Kaden Scott <kscottdev@gmail.com>
2021-06-20 13:11:29 -0400

    add card stuff

---
+++ src/lib/StepCard/index.svelte
@@ -0,0 +1,35 @@
+<script>
+    export let title;
+</script>
+
+<section class="card">
+    <div class="card-header">
+        <h2 class="card-title">{ title }</h2>
+    </div>
+    <div class="card-content">
+        <slot></slot>
+    </div>
+</section>
+
+<style>
+    .card {
+        padding: 1em;
+        display: flex;
+        flex-direction: column;
+        width: 80em;
+        background-color: var(--bg-light-alt);
+        box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.3);
+        position: relative;
+        margin-top: 2em;
+        margin-bottom: 2em;
+    }
+
+    .card-header {
+        margin-bottom: 1em;
+    }
+
+    .card-title {
+        margin: 0;
+        font-weight: normal;
+    }
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,54 @@
+<script>
+    import StepCard from '$lib/StepCard/index.svelte'
+</script>
+
+<StepCard title="📝 Enter your server's details">
+    <div class="forms">
+        <form class="form">
+            <label class="label" for="filename">Your server's .jar filename</label>
+            <input id="filename" class="input" type="text" placeholder="server.jar">
+            <p class="help help-filename"><small>You might be missing '.jar'</small></p>
+        </form>
+        <form class="form">
+            <label class="label" for="ram">RAM allocation</label>
+            <input id="ram" class="input" type="text" placeholder="10G, 2048M">
+            <p class="help help-ram"><small>You might be missing the memory size. M = megabytes, G = gigabytes.</small></p>
+        </form>
+    </div>
+</StepCard>
+
+<StepCard title="💾 Copy your start command">
+    <p>Use this command in your shell script (.sh) or batch file (.bat).</p>
+</StepCard>
+
+<style>
+    .forms {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        padding: 0.2em;
+    }
+
+    .form {
+        display: flex;
+        flex-direction: column;
+        flex: 1;
+        margin-inline: 1em;
+    }
+
+    .input {
+        margin: 0.3em 0;
+        border: 1px solid rgb(170, 170, 170);
+        border-radius: 0.2em;
+        padding: 0.3em;
+        font-size: 1em;
+    }
+
+    .input:focus {
+        border: 1px solid var(--bg-light-alt)
+    }
+
+    .help {
+        margin: 0;
+    }
+</style>
