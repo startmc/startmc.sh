a2c826014431236486a50070affe87782426a4b2
Kaden Scott <kscottdev@gmail.com>
2021-06-21 05:11:15 -0400

    change form positioning

--- src/lib/IndexPage/Step1.svelte
+++ src/lib/IndexPage/Step1.svelte
@@ -4,19 +4,23 @@
 
 <StepCard title="📝 Enter your server's details">
         <form class="form" id="serverForm">
-            <div class="form-section">
-                <label class="label" for="filename"><strong>.jar file</strong></label>
-                <input name="filename" id="filename" class="input" type="text" placeholder="server.jar">
+            <div class="form-column">
+                <div class="form-section">
+                    <label class="label" for="filename"><strong>.jar file</strong></label>
+                    <input name="filename" id="filename" class="input" type="text" placeholder="server.jar">
+                </div>
+                <div class="form-section">
+                    <label class="label" for="ram"><strong>RAM allocation</strong></label>
+                    <input name="ram" id="ram" class="input" type="text" placeholder="10G, 2048M">
+                </div>
             </div>
-            <div class="form-section">
-                <label class="label" for="ram"><strong>RAM allocation</strong></label>
-                <input name="ram" id="ram" class="input" type="text" placeholder="10G, 2048M">
-            </div>
-            <div class="form-section">
-                <label class="label" for="pterodactyl"><strong>Pterodactyl Panel</strong></label>
-                <div class="checkbox-container">
-                    <input name="pterodactyl" id="pterodactyl" type="checkbox">
-                    <p class="subtext">This checkbox fixes an out-of-memory error when using these flags with Pterodactyl when checked.</p>
+            <div class="form-column">
+                <div class="form-section">
+                    <div class="checkbox-container">
+                        <input name="pterodactyl" id="pterodactyl" type="checkbox">
+                        <label class="label" for="pterodactyl"><strong>Pterodactyl Panel</strong></label>
+                    </div>
+                    <p class="subtext">When checked, this fixes an out-of-memory error when using these flags with Pterodactyl.</p>
                 </div>
             </div>
         </form>
@@ -27,14 +31,20 @@
         display: flex;
         flex-direction: row;
         flex-wrap: wrap;
-        justify-content: space-between;
+        justify-content: flex-start;
         padding: 0.2em;
+    }
+
+    .form-column {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-evenly;
+        width: 20em;
     }
 
     .form-section {
         display: flex;
         flex-direction: column;
-        flex: 1;
         margin-inline: 1em;
         margin-bottom: 1em;
     }
@@ -53,21 +63,24 @@
 
     .checkbox-container {
         display: flex;
-        flex-direction: column;
+        flex-direction: row;
         flex: 1;
-        align-items: center;
-        justify-content: center;
+        justify-content: space-evenly;
     }
 
     .checkbox-container input {
-        margin: 0.3em 0;
         border: 1px solid rgb(170, 170, 170);
         border-radius: 0.2em;
-        padding: 0.3em;
+        width: 1.25em;
+        height: 1.25em;
         font-size: 1em;
     }
 
     .input:focus {
         border: 1px solid var(--bg-light-alt)
+    }
+
+    .subtext {
+        text-align: center;
     }
 </style>
