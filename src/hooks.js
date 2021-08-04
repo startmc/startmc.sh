af544b729b87a27d5c9bf21bbb6e8fb555f28781
Kaden Scott <kscottdev@gmail.com>
2021-08-03 22:09:56 -0400

    fix hooks.js error

--- src/hooks.js
+++ src/hooks.js
@@ -1,11 +1,21 @@
 
 /** @type {import('@sveltejs/kit').GetSession} */
 export function getSession(request) {
+    let filename = "server.jar"
+    let ram = ""
+    let pterodactyl = "off"
+
+    if (request.body) {
+        filename = request.body?.get("filename")
+        ram = request.body?.get("ram")
+        pterodactyl = request.body?.get("pterodactyl")
+    }
+
     return {
         server: {
-            filename: request.body?.get("filename") || "",
-            ram: request.body?.get("ram") || "",
-            pterodactyl: request.body?.get("pterodactyl") === "on"
+            filename,
+            ram,
+            pterodactyl
         }
     }
 }
